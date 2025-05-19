ce75dfe168be4b9b0d0fff891287472bb77a8351
adi <adi.takhar@gmail.com>
2025-05-19 12:56:35 -0400

    Cleaned it up

--- main.py
+++ main.py
@@ -19,24 +19,20 @@ try:
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
-except exception as e:
+except Exception as e:
     print(f"Got an error: {e}")
     exit()
 
-
-# define the on-ready event
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    channel_id = 1372335534940098727  # Replace with your channel ID
+    channel_id = 1372335534940098727  
     channel = client.get_channel(channel_id)
     if channel:
         await channel.send('Bot connected!')
     else:
         print("Channel not found.")
 
-# define the on-message event
-# This event is triggered when a message is sent in a channel
 @client.event
 async def on_message(message):
     print(f'Message from {message.author}: {message.content}')
@@ -50,10 +46,7 @@ async def on_message(message):
     elif message.content.startswith(f"<@{client.user.id}>"):
          await message.channel.send('Hello there, You are talking to me!')
 
-    #else: 
-        #await message.channel.send('Hello there')
 
-# now finally run the bot
 try:
     client.run(token)
 except Exception as e:
